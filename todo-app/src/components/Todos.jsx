2c3c1c9a574029919581bf5ed008fedace6ba861
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-12-11 16:34:35 +0530

    updated edit and delete function

--- todo-app/src/components/Todos.jsx
+++ todo-app/src/components/Todos.jsx
@@ -6,6 +6,7 @@ import TodoItem from './TodoItem'
 
 const Todos = () => {
   const [data,setData]=useState([])
+  const [editingItem, setEditingItem] = useState(null);
   
   useEffect(()=>{
     axios('https://jsonplaceholder.typicode.com/users/1/todos')
@@ -20,19 +21,66 @@ const Todos = () => {
     })
   },[])
  
+  const handleDelete = (id) => {
+    const deleteTodo = data.filter((e) => e.id !== id);
+    setData(deleteTodo);
+  };
+
+  const handleEdit = (el) => {
+    setEditingItem(el);
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const updatedData = data.map((item) =>
+      item.id === editingItem.id ? editingItem : item
+    );
+    setData(updatedData);
+    setEditingItem(null);
+  };
+
+  const handleToggle = (Taskid) => {
+    setData((prevTasks) =>
+      prevTasks.map((el) =>
+        el.id === Taskid ? { ...el, isComplete: !el.isComplete } : el
+      )
+    );
+  };
+
+
   console.log(data)
  
 
   return (
     <div>
-      <TodoItem/>
+        <TodoItem/>
+       {editingItem && (
+          <form onSubmit={handleUpdate}>
+            <input
+              type="text"
+              value={editingItem.title}
+              onChange={(e) =>
+                setEditingItem({
+                  ...editingItem,
+                  title: e.target.value,
+                })
+              }
+            />
+            <button className={style.addBtn} type="submit">
+              Update
+            </button>
+          </form>
+        )}
+    
       {data.map((el)=>{
         return<div className={style.main}>
        
-          <p>{el.title}</p>
-          <button className={style.editBtn}><i class="fa-regular fa-pen-to-square"></i></button>
-          <button className={style.delBtn}><i class="fa-solid fa-trash"></i></button>
-        
+          <p style={{ background: el.isComplete ? "green" : "none " }}>{el.title}</p>
+          <button onClick={() => handleEdit(el)} className={style.editBtn}><i class="fa-regular fa-pen-to-square"></i></button>
+          <button onClick={() => handleDelete(el.id)} className={style.delBtn}><i class="fa-solid fa-trash"></i></button>
+          <button onClick={() => handleToggle(el.id)}>
+                  {el.isComplete ? "Incomplete" : "Completed"}
+                </button>
         </div>
       })}
     </div>
